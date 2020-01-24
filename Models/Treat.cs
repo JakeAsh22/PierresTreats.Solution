477e0d844da11b4006e142f1e1444aafdcd3febf
Jake Ash <jacob.ash1998@gmail.com>
2020-01-24 12:34:13 -0800

    Remove unneeded columns from treat table, fix some styling

--- Models/Treat.cs
+++ Models/Treat.cs
@@ -11,8 +11,6 @@ namespace PierresTreats.Models
 
         public int TreatId { get; set; }
         public string Name { get; set; }
-        public string Instructions { get; set; }
-        public string Ingredients { get; set; }
         public virtual ApplicationUser User { get; set; }
         public virtual ICollection<TreatFlavor> Flavors { get; set; }
     }
